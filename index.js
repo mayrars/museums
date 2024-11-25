5f200c6b5dce449ed99df23ad8e0a7b5fa25e354
mayrars <mayra203@gmail.com>
2024-11-25 07:10:22 -0600

    Created const for port and create route for v1

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const v1Router = require("./v1/routes");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1", v1Router)
-
-app.listen(process.env.PORT || 3000), () => {
+app.use("/api/v1", v1Router);
+app.listen(PORT), () => {
     console.log('listen...')
 }
