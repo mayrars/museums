088fdb8f92c9b0f45dc1e5ebdfd596a37f24ea48
mayrars <mayra203@gmail.com>
2024-11-26 05:47:29 -0600

    Created museum service

---
+++ services/museumService.js
@@ -0,0 +1,15 @@
+const getAllMuseums = () => {
+    return;
+}
+const getSingleMuseum = () => {
+    return;
+}
+const createMuseum = () => {
+    return;
+}
+const updateMuseum = () => {
+    return;
+}
+const deleteMuseum = () => {
+    return;
+}
