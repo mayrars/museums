407d4954ff409b43a13b3af27aeb09a272f2ec2f
mayrars <mayra203@gmail.com>
2024-11-27 07:09:00 -0600

    Importando el servicio

--- controllers/museumController.js
+++ controllers/museumController.js
@@ -1,20 +1,26 @@
+const museumService = require("../../services/museumService")
 const getAllMuseums = (req, res) => {
+    const allMuseums = museumService.getAllMuseums();
     res.send("Get all museums")
 }
 
 const getSingleMuseum = (req, res) => {
+    const singleMuseum = museumService.getSingleMuseum(req.params.id);
     res.send("Get museum")
 }
 
 const createMuseum = (req, res) => {
+    const cretedMuseum = museumService.createMuseum(req.params.id);
     res.send("Create museum")
 }
 
 const updateMuseum = (req, res) => {
+    const updatedMuseum = museumService.updateMuseum(req.params.id);
     res.send("Update museum")
 }
 
 const deleteMuseum = (req, res) => {
+    museumService.deleteMuseum(req.params.id);
     res.send("Delete museum")
 }
 
