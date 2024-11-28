2c75ada52fcee864dcb73012f038e17f5a5550f5
mayrars <mayra203@gmail.com>
2024-11-28 07:12:39 -0600

    Installed mongoose

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const v1MuseumsRouter = require("./v1/routes/museumsRouter");
+const mongose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+mongose.connect(process.env.MONGO_URI).then(() => {
+    console.log('connected to db')
+}).catch((err) => {
+    console.log(err)
+})
 
 app.use("/api/v1/museums", v1MuseumsRouter);
 app.listen(PORT), () => {
