d9c2a13734ef61bc1a1e66e779e38d519646f732
mayrars <mayra203@gmail.com>
2024-11-29 07:13:04 -0600

    Crteate validator

---
+++ middlewares/validator.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+exports.createMuseumSchema = Joi.object({
+	name: Joi.string()
+		.min(6)
+		.max(60)
+		.required(),
+	address: Joi.string()
+		.min(6)
+		.max(600)
+		.required(),
+	description: Joi.string()
+		.min(6)
+		.max(600)
+		.required()	,
+    image: Joi.string()
+		.min(6)
+		.max(600),
+    latitud: Joi.number()
+		.min(-90)
+		.max(90),
+    longitud: Joi.number()
+		.min(-180)
+		.max(180),
+    city: Joi.string()
+		.min(6)
+		.required(),
+    country: Joi.string()
+		.min(6)
+		.required()
+})
