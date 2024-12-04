c5de3807bff79da4e6e10a0bcec427f0a2433b4a
mayrars <mayra203@gmail.com>
2024-12-04 07:09:19 -0600

    adding validation

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -28,3 +28,33 @@ exports.createMuseumSchema = Joi.object({
 		.min(6)
 		.required()
 })
+
+exports.updateMuseumSchema = Joi.object({
+	name: Joi.string()
+		.min(6)
+		.max(60)
+		.required(),
+	address: Joi.string()
+		.min(6)
+		.max(600)
+		.required(),
+	description: Joi.string()
+		.min(6)
+		.max(600)
+		.required()	,
+    image: Joi.string()
+		.min(6)
+		.max(600),
+    latitud: Joi.number()
+		.min(-90)
+		.max(90),
+    longitud: Joi.number()
+		.min(-180)
+		.max(180),
+    city: Joi.string()
+		.min(6)
+		.required(),
+    country: Joi.string()
+		.min(6)
+		.required()
+})
