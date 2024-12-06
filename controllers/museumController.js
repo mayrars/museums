36af1a476686f1a1a013339211d093d8de9c456f
mayrars <mayra203@gmail.com>
2024-12-06 07:04:26 -0600

    created search museum function

--- controllers/museumController.js
+++ controllers/museumController.js
@@ -2,7 +2,6 @@ const museumService = require("../services/museumService")
 const { createMuseumSchema, updateMuseumSchema } = require('../middlewares/validator');
 const Museum = require('../models/museumsModel')
 const getAllMuseums = async(req, res) => {
-    //const allMuseums = museumService.getAllMuseums();
     const {page} = req.query
     const museumsPerPage = 10
     try{
@@ -99,6 +98,49 @@ const deleteMuseum = async(req, res) => {
 	}
 }
 
+
+const searchMuseum = async(req, res) => {
+    try{
+        const query = req.query.q || '';
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+
+        const filter = {
+            name: req.query.name,
+            address: req.query.address,
+            description: req.query.description,
+            country: req.query.country,
+            city: req.query.city
+        }
+        const searchData = {}
+        if(query){
+            searchData.$text = {$search: query}
+        }
+        //Apply filters
+        if(filter.name) searchData.name = {$regex: filter.name, $options: 'i'};
+        if(filter.address) searchData.address = {$regex: filter.address, $options: 'i'};
+        if(filter.description) searchData.description = {$regex: filter.description, $options: 'i'};
+        if(filter.country) searchData.country = {$regex: filter.country, $options: 'i'};
+        if(filter.city) searchData.city = {$regex: filter.city, $options: 'i'};
+
+        const museums = await Museum.find(searchData)
+            .sort({ createdAt: -1 })
+            .skip((page - 1) * limit)
+            .limit(limit)
+        const totalMuseums = await Museum.countDocuments(searchData);
+        res.status(200).json({
+            success: true,
+            message: 'Museums found',
+            data: museums,
+            total: totalMuseums,
+            page,
+            limit
+        })
+
+    }catch(error){
+        console.log(error)
+    }
+}
 module.exports = {
     getAllMuseums,
     getSingleMuseum,
